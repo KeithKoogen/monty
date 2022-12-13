4cb67f87a2094c18769612c1aabcf378b07c0a30
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-13 19:37:10 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -5,30 +5,67 @@
 
 char buffer[1024];
 
+void function_selector(char *str)
+{
+	char *token, *line;
+	instruction_t functions[] = {
+	
+		{"push", push()},
+		{"pall", pall()},
+		{NULL, NULL}
+	};
+	
+	line = strtok(buffer, "\n);
+		      print("%s\n", line);
+	token = strtok(line, " ");
+		      print("%s\n", token);
+	
+}
+
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node;
-
-	if (*stack == NULL)
+	stack_t *new_node, *tmp;
+	
+	tmp = *stack;
+	
+	if (tmp == NULL)
 	{
 		new_node = malloc(sizeof(stack_t));
 		new_node->next = NULL;
 		new_node->prev = NULL;
 		*stack = new_node;
 	}
+	else
+	{
+		new_node = malloc(sizeof(stack_t));
+		new_node->next = *stack;
+		new_node->prev = NULL;
+		tmp->prev = new_node;
+		*stack = new_node;
+	}
 }
 
-void printbuffer(char *buffer)
+void pall(stack_t **stack, unsigned int line_number)
 {
-	printf("from buffer: %s\n", buffer);
+	stack_t *tmp;
+	
+	tmp = *stack;
+	
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-
 	char *token;
 	unsigned int line_number;
+	stack_t **stack;
+	
+	*stack = NULL;
 	
 	if (argc < 2 || argc > 2)
 	{
@@ -44,7 +81,8 @@ int main(int argc, char *argv[])
 	}
 	memset(buffer, 0, sizeof(buffer));
 	fread(buffer, sizeof(buffer), 1, fp);
-	printbuffer(buffer);
+	function_selector(buffer);
+	
 
 
 
