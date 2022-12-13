7c3b4c99a16f189c6b93f93137180ad9950f6ec9
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-13 11:06:53 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -5,10 +5,20 @@
 
 int main(int argc, char *argv[])
 {
+	FILE *fp;
+	
 	if (argc < 2 || argc > 2)
 	{
 		printf("%d\n", argc);
 		perror("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		perror("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
 }
