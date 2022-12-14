ebd34f15d7c31c644f8344d2a06a233b91990f59
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-14 13:53:46 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
 	
 	while(fgets(buffer,sizeof(buffer),fp)!= NULL)
 	{
-		function_selector(buffer, stack, line_number)
+		function_selector(buffer, stack, line_number);
       	printf("%s",buffer);
 		++line_number;
     	}
