d76625eafa502396ee5afa540e4a673cf0c00f01
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-12-14 19:07:35 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -282,6 +282,35 @@ void mod(stack_t **stack, unsigned int line_number)
 	}
 }
 
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	
+	tmp = *stack;
+	
+	if (tmp != NULL)
+	{
+		if (tmp->n >= 0 && tmp->n <= 127)
+		{
+			printf("%c\n", tmp->n);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number, token);
+			free_stack(*stack);
+			free(stack);
+			exit(EXIT_FAILURE);			
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+			free_stack(*stack);
+			free(stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
 void function_selector(char *str, stack_t **stack, unsigned int line_number)
 {
 	char *token;
@@ -353,35 +382,6 @@ void function_selector(char *str, stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	
-}
-
-void pchar(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
-	
-	tmp = *stack;
-	
-	if (tmp != NULL)
-	{
-		if (tmp->n >= 0 && tmp->n <= 127)
-		{
-			printf("%c\n", tmp->n);
-		}
-		else
-		{
-			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number, token);
-			free_stack(*stack);
-			free(stack);
-			exit(EXIT_FAILURE);			
-		}
-	}
-	else
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-			free_stack(*stack);
-			free(stack);
-		exit(EXIT_FAILURE);
-	}
 }
 
 
